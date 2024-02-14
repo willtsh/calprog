81ad14199a8c2771a0e9ed78ad2d7822e12a82a6
William Shaw <willtshaw@gmail.com>
2024-02-14 00:42:38 -0500

    start of routing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,18 @@
 'use client';
 import React from 'react';
 import './navbar.css';
+import { useRouter } from "next/navigation";
 
 function Navbar () {
+    const router = useRouter();
     return (
         <nav>
             <div>
                 <ul className="tabs">
-                    <li><a href="./Home">Vertical Push</a></li>
-                    <li><a href="./Home">Vertical Pull</a></li>
-                    <li><a href="./Home">Horizontal Push</a></li>
-                    <li><a href="./Home">Horizontal Pull</a></li>
+                    <li><button type="button" onClick={() => router.push('/vpush')}>Vertical Push</button></li>
+                    <li><button type="button" onClick={() => router.push('/vpull')}>Vertical Pull</button></li>
+                    <li><button type="button" onClick={() => router.push('/hpush')}>Horizontal Push</button></li>
+                    <li><button type="button" onClick={() => router.push('/vpull')}>Horizontal Pull</button></li>
                 </ul>
             </div>
             <div>
---
+++ src/app/vpush/vpush.jsx
@@ -0,0 +1,8 @@
+import "./vpush.css";
+import Navbar from "../../Components/Navbar";
+
+export default function Page() {
+    return (
+        <Navbar />
+    )
+}
